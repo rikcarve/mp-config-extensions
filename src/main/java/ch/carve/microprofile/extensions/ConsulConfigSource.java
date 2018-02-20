9673f387c3d3782233a1fd0adc995c2a69eb59e4
rikcarve <arik.dasen@outlook.com>
2018-02-20 21:05:56 +0100

    very simple working example

--- src/main/java/ch/carve/microprofile/extensions/ConsulConfigSource.java
+++ src/main/java/ch/carve/microprofile/extensions/ConsulConfigSource.java
@@ -8,11 +8,12 @@ import java.util.concurrent.ConcurrentMap;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.kv.model.GetValue;
 
 public class ConsulConfigSource implements ConfigSource {
 
     private ConcurrentMap<String, String> cache = new ConcurrentHashMap<>();
-    private ConsulClient client = new ConsulClient(getEnvOrSystemProperty("consul.host", "localhost"));
+    private ConsulClient client = new ConsulClient("localhost");
 
     @Override
     public Map<String, String> getProperties() {
@@ -21,7 +22,13 @@ public class ConsulConfigSource implements ConfigSource {
 
     @Override
     public String getValue(String propertyName) {
-        return cache.computeIfAbsent(propertyName, k -> client.getKVValue(k).getValue().getDecodedValue());
+        return cache.computeIfAbsent(propertyName, k -> {
+            GetValue value = client.getKVValue(k).getValue();
+            if (value == null) {
+                return null;
+            }
+            return value.getDecodedValue();
+        });
     }
 
     @Override
@@ -35,6 +42,6 @@ public class ConsulConfigSource implements ConfigSource {
     }
 
     private static String getEnvOrSystemProperty(String key, String defaultValue) {
-        return Optional.of(System.getenv(key)).orElse(System.getProperty(key, defaultValue));
+        return Optional.ofNullable(System.getenv(key)).orElse(System.getProperty(key, defaultValue));
     }
 }
