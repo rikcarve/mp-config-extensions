a1f37bdf5338f293b4a98989099f4269777ceffc
rikcarve <arik.dasen@outlook.com>
2018-02-21 20:42:31 +0100

    added caching (default 10s)

--- src/main/java/ch/carve/microprofile/extensions/ConsulConfigSource.java
+++ src/main/java/ch/carve/microprofile/extensions/ConsulConfigSource.java
@@ -3,7 +3,7 @@ package ch.carve.microprofile.extensions;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
@@ -12,23 +12,39 @@ import com.ecwid.consul.v1.kv.model.GetValue;
 
 public class ConsulConfigSource implements ConfigSource {
 
-    private ConcurrentMap<String, String> cache = new ConcurrentHashMap<>();
-    private ConsulClient client = new ConsulClient("localhost");
+    private Map<String, TimedEntry> cache = new ConcurrentHashMap<>();
+    private ConsulClient client = new ConsulClient(getEnvOrSystemProperty("consul.host", "localhost"));
+    private long validity = Long.valueOf(getEnvOrSystemProperty("consul.configsource.validity", "10")) * 1000L;
 
     @Override
     public Map<String, String> getProperties() {
-        return cache;
+        return cache.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey(),
+                        e -> e.getValue().getValue()));
     }
 
     @Override
     public String getValue(String propertyName) {
-        return cache.computeIfAbsent(propertyName, k -> {
-            GetValue value = client.getKVValue(k).getValue();
+        TimedEntry entry = cache.get(propertyName);
+        if (entry == null || entry.isExpired()) {
+            System.out.println("get from consul: " + propertyName);
+            GetValue value = null;
+            try {
+                value = client.getKVValue(propertyName).getValue();
+            } catch (Exception e) {
+                // log
+            }
             if (value == null) {
+                cache.put(propertyName, new TimedEntry(null));
                 return null;
             }
-            return value.getDecodedValue();
-        });
+            String decodedValue = value.getDecodedValue();
+            cache.put(propertyName, new TimedEntry(decodedValue));
+            return decodedValue;
+        }
+        return entry.getValue();
     }
 
     @Override
@@ -43,5 +59,23 @@ public class ConsulConfigSource implements ConfigSource {
 
     private static String getEnvOrSystemProperty(String key, String defaultValue) {
         return Optional.ofNullable(System.getenv(key)).orElse(System.getProperty(key, defaultValue));
+    }
+
+    class TimedEntry {
+        private final String value;
+        private final long timestamp;
+
+        public TimedEntry(String value) {
+            this.value = value;
+            this.timestamp = System.currentTimeMillis();
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public boolean isExpired() {
+            return (timestamp + validity) < System.currentTimeMillis();
+        }
     }
 }
